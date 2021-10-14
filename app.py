56f4371f55eb2d48666a3c5729d7efefa5db4483
Jd2607 <alarconmozo07@gmail.com>
2021-10-13 23:18:26 -0500

    Cambio funcion login

--- app.py
+++ app.py
@@ -15,9 +15,33 @@ def vuelos():
 def destinos():
     return render_template('index.html')
 
-@app.route('/inicio-sesion', methods=['GET'])
+
+@app.route('/inicio-sesion', methods=['GET', 'POST'])
 def login():
+    if (request.method == 'POST'):
+        email_recibido = request.form['correo']
+        contrasenia_recibida = request.form['contrasenia']
+
+        email_admin = 'admin@gmail.com'
+        contrasenia_admin = '123456'
+
+        email_piloto = 'piloto@gmail.com'
+        contrasenia_piloto = '123456'
+
+        email_usuario_final = 'final@gmail.com'
+        contrasenia_usuario_final = '123456'
+
+        if (email_recibido == email_usuario_final and contrasenia_recibida == contrasenia_usuario_final):
+            return render_template('index.html')
+
+        if (email_recibido == email_piloto and contrasenia_recibida == contrasenia_piloto):
+            return render_template('vuelos.html')
+
+        if (email_recibido == email_admin and contrasenia_recibida == contrasenia_admin):
+            return render_template('configuracion_plataforma_usuario.html')
+ 
     return render_template('login.html')
+
 
 @app.route('/registrarse', methods=['GET','POST'])
 def registrarse():
@@ -54,30 +78,7 @@ def comentarios_admin():
 def usuarios_registrados():
     return render_template('usuarios_registrados.html')
 
-@app.route('/validar-login', methods=['POST'])
-def validarlogin():
-    email_recibido = request.form['correo']
-    contrasenia_recibida = request.form['contrasenia']
 
-    email_admin = 'admin@gmail.com'
-    contrasenia_admin = '123456'
-
-    email_piloto = 'piloto@gmail.com'
-    contrasenia_piloto = '123456'
-
-    email_usuario_final = 'final@gmail.com'
-    contrasenia_usuario_final = '123456'
-
-    if (email_recibido == email_usuario_final and contrasenia_recibida == contrasenia_usuario_final):
-        return render_template('index.html')
-
-    if (email_recibido == email_piloto and contrasenia_recibida == contrasenia_piloto):
-        return render_template('vuelos.html')
-
-    if (email_recibido == email_admin and contrasenia_recibida == contrasenia_admin):
-        return render_template('configuracion_plataforma_usuario.html')
-
-    return render_template('login.html')
 
 
 app.run(port = 3000)
