0b8c7bda49a9e4eb4271ee994abb616886667cb4
Jd2607 <alarconmozo07@gmail.com>
2021-10-13 22:19:25 -0500

    Funcion controladora registro

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from logging import debug
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
@@ -19,9 +19,22 @@ def destinos():
 def login():
     return render_template('login.html')
 
-@app.route('/registrarse')
-def registrase():
-    return render_template('registrarse_usuario.html')
+@app.route('/registrarse', methods=['GET','POST'])
+def registrarse():
+    if (request.method == 'GET'):
+        return render_template('registrarse_usuario.html')
+    elif (request.method == 'POST'):
+        nombre = request.form['nombre']
+        contacto = request.form['contacto']
+        email = request.form['email']
+        contraseña = request.form['contraseña']
+        confirmarContraseña = request.form['confirmarContraseña']
+
+        if len(nombre == 0) or len(contacto == 0) or len(email == 0) or len(contraseña == 0) or len(confirmarContraseña == 0):
+            mensaje = "Debe completar todos los campos"
+            return render_template('registrarse_usuario.html', mensaje)
+
+        return redirect(url_for('registrarse_usuario.html'))
 
 @app.route('/plataforma-usuario-verificar')
 def plataforma_usuario_verificar():
